37cf8deafaeab30c1334b276ae6617c4d4da38d5
Robrecht Cannoodt <rcannood@gmail.com>
2024-04-25 15:27:47 +0200

    tweaks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -136,7 +136,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3"
+#define CUSTOM_MACHINE_NAME "Printerboi"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1086,7 +1086,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 440 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 426 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1377,7 +1377,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -10, -3.65 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -10, -2.81 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
